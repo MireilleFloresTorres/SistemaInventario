abad2e6fbdbff1a58fc1361fee399a906f31bb10
MireilleFloresTorres <m.flores.wor@gmail.com>
2025-11-26 15:46:49 -0600

    Implementacion del GenerarTicket
    Se manejan nuevas herramientas para poder guardar en .txt el ticket generado, proximamente le conectaremos a la gasolinera

--- SistemaInventario/SistemaInventario/source/Ticket/GenerarTicket.cpp
+++ SistemaInventario/SistemaInventario/source/Ticket/GenerarTicket.cpp
@@ -9,23 +9,36 @@ GenerarTicket::getNum() {
 	return contadorTickets++; 
 }
 
-bool GenerarTicket::generarArchivo(const Ticket& ticket) {
+bool 
+GenerarTicket::generarArchivo(const Ticket& ticket) {
     std::string nombreArchivo = "ticket_" + std::to_string(ticket.getnumTicket()) + ".txt";
 
     std::ofstream archivo(nombreArchivo);
+    //segun escribimos un archivo con esto
+    //creamos un archivo 
+    //ifstream deberíamos leerlo
     if (!archivo.is_open()) {
         std::cout << " Ha ocurrido un error al crear un ticket" << std::endl;
         return false;
     }
 
-    archivo << "       FARMACIA GUADALAJARA\n";
-    archivo << "Ticket: " << ticket.getnumTicket<< "\n";
+    archivo << " FARMACIA GUADALAJARA\n";
+    archivo << "Ticket: " << ticket.getnumTicket()<< "\n";
 
     archivo << "Fecha: " << ticket.getFecha() << " " << ticket.getHora() << "\n";
 
     for (const auto& product : ticket.getProducts()) {
         archivo << product.nameProduct<< "\n";
         archivo <<"Cantidad: "<<product.cantidad<< product.precioU << "\n";
-        archivo <<
+        archivo <<"Subtoal: "<<product.subtotal<< "\n";
     }
+
+    archivo << "Total: " << ticket.getTotal() << "\n";
+    archivo << "Pago: " << ticket.getMetodoPago() << "\n";
+
+    archivo.close();
+    //esto es para cerrar el txt que ojala sisirva 
+    //guarda todo
+    std::cout << "Ticket generado: " << nombreArchivo << std::endl;
+    return true;
 }
